9c972ef8798fb7d45dfcccf74570fce9cb6bd7a0
Papa Nii Vanderpuye <pvanderpuye@healthverity.com>
2019-11-24 21:35:23 -0500

    Updates

---
+++ precache-manifest.efc1d6b52d5ee42e666f96ee10dce8e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9f30643f4d0081dba6c10e940b593e",
+    "url": "/modello/index.html"
+  },
+  {
+    "revision": "066f9d8adb47bee72c99",
+    "url": "/modello/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "450d51f37eb4e3fe9fd9",
+    "url": "/modello/static/js/2.386db043.chunk.js"
+  },
+  {
+    "revision": "066f9d8adb47bee72c99",
+    "url": "/modello/static/js/main.64816c3b.chunk.js"
+  },
+  {
+    "revision": "bef980313b467f18acb9",
+    "url": "/modello/static/js/runtime-main.49f3f39f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/modello/static/media/logo.25bf045c.svg"
+  }
+]);
